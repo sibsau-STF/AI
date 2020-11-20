345604ce441b83fafe8e22e744d03a2af10abc75
Dmitry Frolov <frdima19@gmail.com>
2020-11-20 17:21:30 +0700

    add json formatting rules

--- Lab1_WFApp/Lab1_WFApp/Classes/JSONClassConverter.cs
+++ Lab1_WFApp/Lab1_WFApp/Classes/JSONClassConverter.cs
@@ -7,73 +7,80 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Lab1_WFApp
-	{
+{
 	public static class JSONClassConverter
+	{
+
+		static JsonSerializerSettings settings = new JsonSerializerSettings()
 		{
+			Formatting = Formatting.Indented,
+			NullValueHandling = NullValueHandling.Include
+		};
+
 		public static List<Job> GetJobs (List<Statement> statements)
-			{
+		{
 			List<Job> Jobs;
 			using ( StreamReader fs = new StreamReader(@"jobs.json") )
-				{
+			{
 				string json = fs.ReadToEnd();
-				Jobs = JsonConvert.DeserializeObject<List<Job>>(json)
+				Jobs = JsonConvert.DeserializeObject<List<Job>>(json, settings)
 					.Select(j => j.mapToStatements(statements)).ToList();
 				Jobs.Sort((one, two) => one.Id.CompareTo(two.Id));
 			}
 			return Jobs;
-			}
+		}
 
 		public static void WriteJobs (List<Job> jobs)
-			{
+		{
 			using ( StreamWriter fs = new StreamWriter(@"jobs.json") )
-				{
-				string json = JsonConvert.SerializeObject(jobs);
+			{
+				string json = JsonConvert.SerializeObject(jobs, settings);
 				fs.Write(json);
-				}
 			}
+		}
 
 		public static List<Rule> GetRules (List<Statement> statements)
-			{
+		{
 			List<Rule> rules;
 			using ( StreamReader fs = new StreamReader(@"rules.json") )
-				{
+			{
 				string json = fs.ReadToEnd();
-				rules = JsonConvert.DeserializeObject<List<Rule>>(json)
+				rules = JsonConvert.DeserializeObject<List<Rule>>(json, settings)
 					.Select(j => j.mapToStatements(statements)).ToList();
 				rules.Sort((one, two) => one.Id.CompareTo(two.Id));
-				}
-			return rules;
 			}
+			return rules;
+		}
 
 		public static void WriteRules (List<Rule> rules)
-			{
+		{
 			using ( StreamWriter fs = new StreamWriter(@"rules.json") )
-				{
-				string json = JsonConvert.SerializeObject(rules);
+			{
+				string json = JsonConvert.SerializeObject(rules, settings);
 				fs.Write(json);
-				}
 			}
+		}
 
 		public static List<Statement> GetStatements ()
-			{
+		{
 			List<Statement> states;
 			using ( StreamReader fs = new StreamReader(@"statements.json") )
-				{
+			{
 				string json = fs.ReadToEnd();
-				states = JsonConvert.DeserializeObject<List<Statement>>(json).ToList();
+				states = JsonConvert.DeserializeObject<List<Statement>>(json, settings).ToList();
 				states.Sort((one, two) => one.Id.CompareTo(two.Id));
 			}
 			return states;
-			}
+		}
 
 		public static void WriteStatements (List<Statement> states)
-			{
+		{
 			using ( StreamWriter fs = new StreamWriter(@"statements.json") )
-				{
-				string json = JsonConvert.SerializeObject(states);
+			{
+				string json = JsonConvert.SerializeObject(states, settings);
 				fs.Write(json);
-				}
 			}
-
 		}
+
 	}
+}
